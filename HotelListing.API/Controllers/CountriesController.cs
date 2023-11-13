da189d5f3e177cfd8cfdd65a0e324194858e7885
Salil Shukla <dsalilshukla@gmail.com>
2023-11-13 16:36:19 +0530

    REST API Best Practices, Automapper and Respository

---
+++ HotelListing.API/Controllers/CountriesController.cs
@@ -0,0 +1,117 @@
+﻿using AutoMapper;
+using HotelListing.API.Contracts;
+using HotelListing.API.Data;
+using HotelListing.API.Models.Country;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        private readonly ICountriesRepository _countriesRepository;
+        private readonly IMapper _mapper;
+
+        public CountriesController(ICountriesRepository countriesRepository, IMapper mapper)
+        {
+            //_context = context;
+            _mapper = mapper;
+            _countriesRepository = countriesRepository;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Country>> PostCountry(CreateCountryDto createCountryDto)
+        {
+            var country = _mapper.Map<Country>(createCountryDto);
+
+            await _countriesRepository.AddAsync(country);
+
+            return CreatedAtAction("GetCountry", new { id = country.Id }, country);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<GetCountryDto>>> GetCountries()
+        {
+            //var countries = await _context.Countries.ToListAsync();
+            var countries = await _countriesRepository.GetAllAsync();
+            var records = _mapper.Map<List<GetCountryDto>>(countries);
+            return Ok(records);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CountryDto>> GetCountry(int id)
+        {
+            var country = await _countriesRepository.GetDetails(id);
+
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            var countryDto = _mapper.Map<CountryDto>(country);
+
+            return Ok(countryDto);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCountry(int id, UpdateCountryDto updateCountryDto)
+        {
+            if (id != updateCountryDto.Id)
+            {
+                return BadRequest("Invalid Record Id");
+            }
+
+            //var country = await _context.Countries.FindAsync(id);
+
+            var country = await _countriesRepository.GetAsync(id);
+
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            _mapper.Map(updateCountryDto, country);
+
+            try
+            {
+                await _countriesRepository.UpdateAsync(country);
+
+            }
+
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!await CountryExists(id))
+                {
+                    return NotFound();
+                }
+
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCountry(int id)
+        {
+            var country = await _countriesRepository.GetAsync(id);
+
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            await _countriesRepository.DeleteAsync(id);
+            return NoContent();
+        }
+
+        private async Task<bool> CountryExists(int id)
+        {
+            return await _countriesRepository.Exists(id);
+        }
+    }
+}
--- HotelListing.API/Program.cs
+++ HotelListing.API/Program.cs
@@ -1,4 +1,7 @@
+using HotelListing.API.Configurations;
+using HotelListing.API.Contracts;
 using HotelListing.API.Data;
+using HotelListing.API.Repository;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 
@@ -14,6 +17,7 @@ builder.Services.AddDbContext<HotelListingDbContext>(options =>
 });
 
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(typeof(MapperConfig));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -22,6 +26,9 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", b => b.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
 });
+
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<ICountriesRepository, CountriesRepository>();
 
 var app = builder.Build();
 
